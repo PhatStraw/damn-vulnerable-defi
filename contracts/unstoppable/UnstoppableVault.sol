5ca255b42e155daeff1815a8e4b4790b2c06cad1
PhatStraw <kevindsimsjr@gmail.com>
2023-10-31 16:01:09 -0700

    unstoppable

--- contracts/unstoppable/UnstoppableVault.sol
+++ contracts/unstoppable/UnstoppableVault.sol
@@ -90,16 +90,27 @@ contract UnstoppableVault is IERC3156FlashLender, ReentrancyGuard, Owned, ERC462
         uint256 amount,
         bytes calldata data
     ) external returns (bool) {
+        // @audit-info validation for amount and _token
         if (amount == 0) revert InvalidAmount(0); // fail early
         if (address(asset) != _token) revert UnsupportedCurrency(); // enforce ERC3156 requirement
+        
+        // @audit-info accounting check
         uint256 balanceBefore = totalAssets();
+        //balanceBefore = 1m
+        // convertToShares(totalSupply) = 1m
         if (convertToShares(totalSupply) != balanceBefore) revert InvalidBalance(); // enforce ERC4626 requirement
+        
+        // @audit-info calulate fee and transfer tokens
         uint256 fee = flashFee(_token, amount);
         // transfer tokens out + execute callback on receiver
         ERC20(_token).safeTransfer(address(receiver), amount);
+       
+       //@audit-info execute reciever callback and validate interface / hash
         // callback must return magic value, otherwise assume it failed
         if (receiver.onFlashLoan(msg.sender, address(asset), amount, fee, data) != keccak256("IERC3156FlashBorrower.onFlashLoan"))
             revert CallbackFailed();
+        
+        
         // pull amount + fee from receiver, then pay the fee to the recipient
         ERC20(_token).safeTransferFrom(address(receiver), address(this), amount + fee);
         ERC20(_token).safeTransfer(feeRecipient, fee);
